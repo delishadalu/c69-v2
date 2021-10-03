0effffe08b3723231bac246d06613244d444c3d0
delishadalu <73183936+delishadalu@users.noreply.github.com>
2021-10-03 15:19:22 +0530

    Update Transaction.js

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
-export default class Transacyion extends Component {
+export default class Transaction extends Component {
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: "teal", alignItems: "center", justifyContent: "center" }}>
