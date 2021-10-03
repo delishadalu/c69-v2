6c97e47acb77fcb9fcc43ba6684e18e28ea92b61
delishadalu <73183936+delishadalu@users.noreply.github.com>
2021-10-03 15:04:11 +0530

    Add files via upload

---
+++ screens/Transaction.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class Transacyion extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, backgroundColor: "teal", alignItems: "center", justifyContent: "center" }}>
+                <Text> transaction screen </Text>
+            </View>
+        )
+    }
+}
